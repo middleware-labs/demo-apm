ff01be5168b5099aee90f8f1176dd7eef29061bb
milan03-mw <milan.vaghasiya@middleware.io>
2025-12-03 13:23:52 +0530

    Dotnet version changes and build changes

--- dotnet/dotnet-demo/Mw-WebApplication/Program.cs
+++ dotnet/dotnet-demo/Mw-WebApplication/Program.cs
@@ -1,4 +1,21 @@
+using Middleware.APM;
+
 var builder = WebApplication.CreateBuilder(args);
+              
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables()
+    .Build();
+
+builder.Services.ConfigureMWInstrumentation(configuration);
+
+// builder.Logging.AddConfiguration(configuration.GetSection("Logging"));
+// builder.Logging.AddConsole();
+// builder.Logging.SetMinimumLevel(LogLevel.Debug);
+
+// Add the following code after var app = builder.Build();
+// Logger.Init(app.Services.GetRequiredService<ILoggerFactory>());
 
 // Add services to the container.
 
